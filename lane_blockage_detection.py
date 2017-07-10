4b708c891ed80b6bb06c11e16c65df005d796098
Suzie Petryk <sp762@cornell.edu>
2017-07-09 22:47:55 -0700

    Started outlining algorithm

--- lane_blockage_detection.py
+++ lane_blockage_detection.py
@@ -3,7 +3,7 @@ from network_setup_hardcode import *
 
 
 vehLength = 15.0 	# Average length of vehicle, in feet
-detectorInfoFile = 'NetworkInfoFiles/detectorInfoFile2.csv'
+detectorInfoFile = 'NetworkInfoFiles/detectorInfoFile.csv'
 
 def main():
 	
@@ -12,14 +12,23 @@ def main():
 	# Outline Aimsun network: connections between intersections, approaches, movements, and detectors
 	intersectionDict, detectorDict = initializeNetwork(detectorInfoFile)
 
-	print(detectorDict[100002].category)
-	calculate_critical_occupancies(detectorDict[100002], 'Left')
-	print(detectorDict[100002].criticalOccs)
+
+	# At each time step: 
+		# Estimate traffic state for each movement
+			# Check advanced detectors
+			# 
+			# Average data from each detector in that movement
+
+	# print(detectorDict[200019].category)
+	# calculate_critical_occupancies(detectorDict[200019], 'Left')
+	# print(detectorDict[200019].criticalOccs)
 
 	
 
 
 def calculate_critical_occupancies(detector, turn):
+	# Set critical occupancies for detector: two thresholds for advanced, one threshold for stopbar
+
 	# detector: Detector object
 	# turn: 'Left', 'Through', or 'Right'
 
@@ -35,6 +44,7 @@ def calculate_critical_occupancies(detector, turn):
 	v_sat_adv = detector.movements[turn].satVelocityAdvanced 			# miles per hour
 
 	if detector.category == 'Advanced':
+		# Two critical occupancies
 		occ_crit_1 = ((L+D)*3600 / (v_sat_adv*5280))  * (1.0/h) * (G/C)
 		occ_crit_2 = ((L+D)*3600 / (v_sat_adv*5280))  * (1.0/h) * (G/C) + ((C-G)/C)
 		detector.criticalOccs = [occ_crit_1, occ_crit_2]
@@ -44,6 +54,16 @@ def calculate_critical_occupancies(detector, turn):
 		occ_crit = ((L+D) / (v_sat_sb*5280*3600))  * (1.0/h) + ((C-G)/C)
 		detector.criticalOccs.append(occ_crit)
 
+	# criticalOccs field of given detector object now contains array of critical occupancies
+
+def estimateMovementState(mvmt):
+	# Estimate traffic state of movement
+		# 
+
+	direction = mvmt.direction	# 'Left', 'Through', or 'Right'
+
+	for det in mvmt.detectors:
+		# Average the occupancies from 
 
 
 if __name__ == '__main__':
--- network_setup_hardcode.py
+++ network_setup_hardcode.py
@@ -8,30 +8,30 @@ def initializeNetwork(detectorInfoFile):
 	# ********** Initialize network setup *****************
 
 	# Intersections
-	intersectionDict = {'West': Intersection(externalID = 1000, cycleTime = 90.0), 'East' : Intersection(externalID = 2000, cycleTime = 90.0)}
+	intersectionDict = {1000: Intersection(externalID = 1000, cycleTime = 90.0), 2000 : Intersection(externalID = 2000, cycleTime = 90.0)}
 
 	# Approaches
-	intersectionDict['West'].approaches[828] = Approach(sectionID=828, intersection=intersectionDict['West'])
-	intersectionDict['West'].approaches[825] = Approach(sectionID=825, intersection=intersectionDict['West'])
-	intersectionDict['East'].approaches = {} # Bug: West approaches were being applied to East as well, so clear East
+	intersectionDict[1000].approaches[828] = Approach(sectionID=828, intersection=intersectionDict[1000])
+	intersectionDict[1000].approaches[825] = Approach(sectionID=825, intersection=intersectionDict[1000])
 
-	intersectionDict['East'].approaches[826] = Approach(sectionID=826, intersection=intersectionDict['East'])
-	intersectionDict['East'].approaches[818] = Approach(sectionID=818, intersection=intersectionDict['East'])
+	intersectionDict[2000].approaches = {} # Bug: 1000's approaches were being applied to 2000's as well, so clear 2000 before assigning
+	intersectionDict[2000].approaches[826] = Approach(sectionID=826, intersection=intersectionDict[2000])
+	intersectionDict[2000].approaches[818] = Approach(sectionID=818, intersection=intersectionDict[2000])
 
 
 	# Movements
-	intersectionDict['West'].approaches[828].movements = {'Left': Movement(direction='Left',greenTime=20.0,satVelocityStopbar=20.0,satVelocityAdvanced=30,headway=2.3,approach=intersectionDict['West'].approaches[828]),
-													'Through': Movement(direction='Through',greenTime=50.0,satVelocityStopbar=25.0,satVelocityAdvanced=30,headway=2.3,approach=intersectionDict['West'].approaches[828]),
-													 'Right': Movement(direction='Right',greenTime=20.0,satVelocityStopbar=20.0,satVelocityAdvanced=30,headway=2.3,approach=intersectionDict['West'].approaches[828])}
-	intersectionDict['West'].approaches[825].movements = {'Left': Movement(direction='Left',greenTime=20.0,satVelocityStopbar=20.0,satVelocityAdvanced=30,headway=2.3,approach=intersectionDict['West'].approaches[825]),
-													'Through': Movement(direction='Through',greenTime=50.0,satVelocityStopbar=25.0,satVelocityAdvanced=30,headway=2.3,approach=intersectionDict['West'].approaches[825]),
-													 'Right': Movement(direction='Right',greenTime=20.0,satVelocityStopbar=20.0,satVelocityAdvanced=30,headway=2.3,approach=intersectionDict['West'].approaches[825])}
-	intersectionDict['East'].approaches[826].movements = {'Left': Movement(direction='Left',greenTime=20.0,satVelocityStopbar=20.0,satVelocityAdvanced=30,headway=2.3,approach=intersectionDict['East'].approaches[826]),
-													'Through': Movement(direction='Through',greenTime=50.0,satVelocityStopbar=25.0,satVelocityAdvanced=30,headway=2.3,approach=intersectionDict['East'].approaches[826]),
-													 'Right': Movement(direction='Right',greenTime=20.0,satVelocityStopbar=20.0,satVelocityAdvanced=30,headway=2.3,approach=intersectionDict['East'].approaches[826])}
-	intersectionDict['East'].approaches[818].movements = {'Left': Movement(direction='Left',greenTime=20.0,satVelocityStopbar=20.0,satVelocityAdvanced=30,headway=2.3,approach=intersectionDict['East'].approaches[818]),
-													'Through': Movement(direction='Through',greenTime=50.0,satVelocityStopbar=25.0,satVelocityAdvanced=30,headway=2.3,approach=intersectionDict['East'].approaches[818]),
-													 'Right': Movement(direction='Right',greenTime=20.0,satVelocityStopbar=20.0,satVelocityAdvanced=30,headway=2.3,approach=intersectionDict['East'].approaches[818])}
+	intersectionDict[1000].approaches[828].movements = {'Left': Movement(direction='Left',greenTime=20.0,satVelocityStopbar=20.0,satVelocityAdvanced=30,headway=2.3,approach=intersectionDict[1000].approaches[828]),
+													'Through': Movement(direction='Through',greenTime=50.0,satVelocityStopbar=25.0,satVelocityAdvanced=30,headway=2.3,approach=intersectionDict[1000].approaches[828]),
+													 'Right': Movement(direction='Right',greenTime=20.0,satVelocityStopbar=20.0,satVelocityAdvanced=30,headway=2.3,approach=intersectionDict[1000].approaches[828])}
+	intersectionDict[1000].approaches[825].movements = {'Left': Movement(direction='Left',greenTime=20.0,satVelocityStopbar=20.0,satVelocityAdvanced=30,headway=2.3,approach=intersectionDict[1000].approaches[825]),
+													'Through': Movement(direction='Through',greenTime=50.0,satVelocityStopbar=25.0,satVelocityAdvanced=30,headway=2.3,approach=intersectionDict[1000].approaches[825]),
+													 'Right': Movement(direction='Right',greenTime=20.0,satVelocityStopbar=20.0,satVelocityAdvanced=30,headway=2.3,approach=intersectionDict[1000].approaches[825])}
+	intersectionDict[2000].approaches[826].movements = {'Left': Movement(direction='Left',greenTime=20.0,satVelocityStopbar=20.0,satVelocityAdvanced=30,headway=2.3,approach=intersectionDict[2000].approaches[826]),
+													'Through': Movement(direction='Through',greenTime=50.0,satVelocityStopbar=25.0,satVelocityAdvanced=30,headway=2.3,approach=intersectionDict[2000].approaches[826]),
+													 'Right': Movement(direction='Right',greenTime=20.0,satVelocityStopbar=20.0,satVelocityAdvanced=30,headway=2.3,approach=intersectionDict[2000].approaches[826])}
+	intersectionDict[2000].approaches[818].movements = {'Left': Movement(direction='Left',greenTime=20.0,satVelocityStopbar=20.0,satVelocityAdvanced=30,headway=2.3,approach=intersectionDict[2000].approaches[818]),
+													'Through': Movement(direction='Through',greenTime=50.0,satVelocityStopbar=25.0,satVelocityAdvanced=30,headway=2.3,approach=intersectionDict[2000].approaches[818]),
+													 'Right': Movement(direction='Right',greenTime=20.0,satVelocityStopbar=20.0,satVelocityAdvanced=30,headway=2.3,approach=intersectionDict[2000].approaches[818])}
 
 	# Detectors
 
@@ -42,7 +42,7 @@ def initializeNetwork(detectorInfoFile):
 		fileReader = csv.reader(detFile, delimiter = ',')
 		next(detFile) # Skip header row
 		for row in fileReader:
-			intrsct = row[0]			# Intersection detector is in
+			intrsct = int(row[0])			# Intersection detector is in
 			sectID = int(row[1])		# Section detector is in
 			extID = int(row[2])			# Detector external ID
 			category = row[3]			# Advanced or stopbar
