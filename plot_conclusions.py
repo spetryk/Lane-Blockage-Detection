bac486385c525cc0fdb436f2e4d27fd4728380bc
Suzie Petryk <sp762@cornell.edu>
2017-08-26 18:33:25 -0400

    Moved time steps into one subplot

--- plot_conclusions.py
+++ plot_conclusions.py
@@ -3,7 +3,8 @@
 import pandas as pd
 import matplotlib
 import matplotlib.pyplot as plt
-import pylab as pl
+from matplotlib.patches import Rectangle
+from matplotlib.ticker import MultipleLocator, FormatStrFormatter
 
 
 class Conclusion():
@@ -20,7 +21,7 @@ class Conclusion():
 			# 5: Lane Blockage
 		self.marker = marker
 			# Marker on scatterplot: 
-				# 'L' for left movement, 'T' for Through, 'R' for right, 'o' otherwise
+				# 'L' for left movement, 'T' for Through, 'R' for right, '.' otherwise
 				# Can be any combination of L,T,R: e.g. 'TR' for Through and Right movements
 		self.QSB = QSB
 
@@ -36,8 +37,8 @@ def main():
 		congLevel, congNumber = determine_congestion(row.Conclusion)
 		movements = [word for word in concl_string.split() if word in movementKeywords]
 		if movements==[]:
-			# No information, or uncongested: use simple 'o' as marker
-			marker = 'o'
+			# No information, or uncongested: use simple '.' as marker
+			marker = '.'
 		else:
 			# Marker is first letter of each movement
 			letters = [word[0] for word in movements]
@@ -76,39 +77,64 @@ def determine_congestion(conclusion):
 def make_plots(allConclusions):
 
 	matplotlib.rcParams.update({'font.size': 18})
-	categories = ('No Information', 'Uncongested', 'Light', 'Moderate', 'Heavy', 'Lane Blockage')
-	y = [0, 1, 2, 3, 4, 5]
 
-	fig1 = pl.figure(1)	# 12 AM - 6 AM
-	x = [0, 7200, 14400, 21600]
-	plt.xticks(x, ('12 AM', '2 AM', '4 AM', '6 AM'))
-	plt.yticks(y, categories)
-	pl.ylim([0, 5.5])
+	# fig = plt.figure()
+	# ax = fig.add_subplot(111)    # The big subplot
+	# ax1 = fig.add_subplot(211)
+	# ax2 = fig.add_subplot(212)
+	# ax3 = fig.add_subplot(213)
 
-	fig2 = pl.figure(2) # 6 AM - 12 PM
-	x = [21600, 28800, 36000, 43200]
-	plt.xticks(x, ('6 AM', '8 AM', '10 AM', '12 PM'))
-	plt.yticks(y, categories)
-	pl.ylim([0, 5.5])
-	
-	fig3 = pl.figure(3)	# 12 PM - 6 PM
-	x = [43200, 50400, 57600, 64800]
-	plt.xticks(x, ('12 PM', '2 PM', '4 PM', '6 PM'))
-	plt.yticks(y, categories)
-	pl.ylim([0, 5.5])
+	fig, axes = plt.subplots(3,1,sharey='col')
+	dummy = Rectangle((0, 0), 1, 1, fc="w", fill=False, edgecolor='none', linewidth=0)
+	red_rec = Rectangle((0, 0), 1, 1, fc="r")
+	leg = fig.legend(handles=[dummy,dummy,dummy,dummy,red_rec],
+	 labels=['$U$    Unknown Downstream Traffic       ', '$L$    Left Movement',
+	  '$T$    Through Movement', '$R$    Right Movement','Queue Spillback'],
+	  loc='upper center', fontsize=16, ncol=3, bbox_to_anchor=(0.5,1)) 
+
+	plt.setp(axes, yticks=[0, 1, 2, 3, 4, 5], 
+		yticklabels=['   No Information', '   Uncongested', '   Light', '   Moderate', '   Heavy', '   Lane Blockage'])
+
+	plt.sca(axes[0])
+	x = [0, 3600, 7200, 10800, 14400, 18000, 21600]
+	plt.xticks(x, ('12 AM', '1 AM', '2 AM', '3 AM', '4 AM', '5 AM', '6 AM'))
+	minorLocator = MultipleLocator(1800)
+	axes[0].xaxis.set_minor_locator(minorLocator)
+	plt.ylim([0,5.5])
+	plt.grid(which='major', linestyle='--')
+	plt.grid(which='minor',axis='x')
+
+	plt.sca(axes[1])
+	x = [21600, 25200, 28800, 32400, 36000, 39600, 43200]
+	plt.xticks(x, ('6 AM', '7 AM', '8 AM', '9 AM', '10 AM', '11 AM', '12 PM'))
+	plt.ylim([0,5.5])
+	minorLocator = MultipleLocator(1800)
+	axes[1].xaxis.set_minor_locator(minorLocator)
+	axes[1].set_ylabel('Congestion Level')
+	plt.grid(which='major', linestyle='--')
+	plt.grid(which='minor',axis='x')
+
+	plt.sca(axes[2])
+	x = [43200, 46800, 50400, 54000, 57600, 61200, 64800]
+	plt.xticks(x, ('12 PM', '1 PM', '2 PM', '3 PM', '4 PM', '5 PM', '6 PM'))
+	plt.ylim([0,5.5])
+	minorLocator = MultipleLocator(1800)
+	axes[2].xaxis.set_minor_locator(minorLocator)
+	axes[2].set_xlabel('Time')
+	plt.grid(which='major', linestyle='--')
+	plt.grid(which='minor',axis='x')
 
 
 	for concl in allConclusions:
-		# Plot only first time category for now
-		if concl.curr_time < 22200:
-			pl.figure(1)
-		elif concl.curr_time < 43800:
-			pl.figure(2)
-		else:
-			pl.figure(3)
-
 		color = 'r' if concl.QSB != None else 'k'
-		plt.scatter(concl.curr_time, concl.congestionNumber, s=90, marker=concl.marker, edgecolors=color, facecolors='k')
+		if concl.curr_time < 22200:
+			plt.sca(axes[0])
+		elif concl.curr_time < 43800:
+			plt.sca(axes[1])
+		else:
+			plt.sca(axes[2])
+
+		plt.scatter(concl.curr_time, concl.congestionNumber, s=150*(len(concl.marker.replace('$',''))), marker=concl.marker, edgecolors=color, facecolors='k')
 	
 	plt.show()
 
